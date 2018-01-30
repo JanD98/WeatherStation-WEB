c6d84ee9a9e5b660804c58798bbc3f76ca67a041
Romy Bos <romy.bos@hotmail.com>
2018-01-30 14:01:54 +0100

    Added fully operating login system.

--- Weatherstation/sealevel/views.py
+++ Weatherstation/sealevel/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/login/')
 def sealevel(request):
     """
     The home page of the weatherstation web app
