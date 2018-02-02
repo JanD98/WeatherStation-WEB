c3991d3ab256acf0651f30a17185f974db7c7a15
Melle Dijkstra <melle210202@gmail.com>
2018-02-02 16:27:44 +0100

    Major bugfix!

--- weatherstation/mainland/urls.py
+++ weatherstation/mainland/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'', views.mainland, name='mainland')
+    url(r'^$', views.mainland, name='mainland'),
+    # Line chart
+    url(r'^line_chart/$', views.line_chart,
+        name='line_chart'),
+    url(r'^line_chart/json/$', views.line_chart_json,
+        name='line_chart_json'),
 ]
--- weatherstation/mainland/views.py
+++ weatherstation/mainland/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from chartjs.views.lines import BaseLineChartView
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
+
 
 @login_required(login_url='/login/')
 def mainland(request):
@@ -10,4 +33,4 @@ def mainland(request):
     :return:
     """
     # TODO: read measurement data from shared file share
-    return render(request, 'mainland/mainland.html')
+    return render(request, 'mainland.html')
